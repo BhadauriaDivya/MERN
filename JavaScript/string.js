4fe88f2cbe17272821356ec52ecccc9a2e495854
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2024-08-01 12:37:00 +0530

    first

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -4,4 +4,14 @@ console.log(hobbies);
 function sum(a, b, c){
     return a + b + c;
 }
+
+let sum = function(a, b, c){
+    return a + b + c;
+}
+
+//Arrow function
+
+let sum=(a,b,c)=>{
+    return a+b+c;
+}
 console.log(sum(1,2,3));
