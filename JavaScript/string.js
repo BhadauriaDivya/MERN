02183fd8d5741629097ae0084f08ec769f19bbde
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2024-08-06 16:45:50 +0530

    first

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -9,9 +9,28 @@ let sum = function(a, b, c){
     return a + b + c;
 }
 
+
 //Arrow function
+
+// { function() } == { ()=> }
 
 let sum=(a,b,c)=>{
     return a+b+c;
 }
 console.log(sum(1,2,3));
+
+const msg1=()=>{
+    console.log("Welcome to arrow function");
+}
+msg1();
+
+const sum=(num1,num2)=>console.log("Sum=",(num1+num2));
+sum(23,56);
+
+const mul=(num1,num2)=>num1*num2;
+console.log("Multiply=",mul(23,56));
+
+const mult=(num1,num2)=>{
+    return num1*num2;
+}
+console.log("Multiply=",mult(23,56));
